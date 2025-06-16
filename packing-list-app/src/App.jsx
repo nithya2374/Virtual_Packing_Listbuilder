298dc35e5d7b28f3b2973c8e8ae628162e72a8a5
Mahamahu22 <mahalakshmirajan07@gmail.com>
2025-06-16 11:47:22 +0530

    added

---
+++ packing-list-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-6 rounded-xl text-center">
+      Tailwind is working 🎉
+    </div>
+  );
+}
+
+export default App;
