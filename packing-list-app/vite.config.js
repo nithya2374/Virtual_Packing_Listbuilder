de1cf50fa0ccc7523a0494b2c8a3b438766089a8
nithya2374 <nithyasri17012004@gmail.com>
2025-06-18 23:43:35 +0530

    Add base

--- packing-list-app/vite.config.js
+++ packing-list-app/vite.config.js
@@ -1,18 +1,14 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react(),],
+  base: '/Virtual_Packing_Listbuilder/', 
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  server: {
-    fs: { strict: false },
-    historyApiFallback: true, // This is the KEY FIX for 404s
   }
 });
 
